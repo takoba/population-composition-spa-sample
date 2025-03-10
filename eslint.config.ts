248dcd8485e359deec8b34c15da44fffc5962213
takoba <plane25@gmail.com>
2025-03-10 15:52:20 +0900

    :heavy_plus_sign: Install eslint plugins & setup them
    - exec `npm install -D @types/eslint @types/eslint__js @typescript-eslint/parser eslint-plugin-import @stylistic/eslint-plugin eslint-plugin-import eslint-import-resolver-typescript eslint-import-resolver-alias`
    - setup eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,16 +3,62 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
+import tsParser from '@typescript-eslint/parser'
+import stylisticPlugin from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
   { ignores: ['dist'] },
+
+  js.configs.recommended,
+  tseslint.configs.recommended,
+
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  importPlugin.flatConfigs.errors,
+
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
     },
+    plugins: {
+      '@stylistic': stylisticPlugin,
+    },
+    rules: {
+      '@stylistic/explicit-module-boundary-types': 'off',
+       '@stylistic/max-len': ['error', {
+         code: 100,
+       }],
+       '@stylistic/no-empty-function': 'off',
+       '@stylistic/no-empty-interface': 'off',
+       '@stylistic/no-trailing-spaces': 'error',
+       '@stylistic/quotes': ['error', 'single'],
+       'no-unused-vars': 'off',
+       'sort-imports': 'off',
+       'import/default': 'off',
+       'import/no-unresolved': 'error',
+       'import/order': [
+         'error',
+         {
+           alphabetize: {
+             order: 'asc',
+           },
+         },
+       ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+  },
+
+  {
+    files: ['src/**/*.{ts,mts,tsx}'],
+
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
@@ -23,6 +69,15 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+    },
+    settings: {
+      'import/resolver': {
+        alias: {
+          map: [
+            ['', './public']
+          ],
+        },
+      },
     },
   },
 )
