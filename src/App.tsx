066af377b40cb5d80107933f48b36a6002a10139
takoba <plane25@gmail.com>
2025-03-22 18:09:39 +0900

    :lipstick: Add normalize.css & fix layout a little

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import 'normalize.css'
 import PrefecturesJsonTest from './components/PrefecturesJsonTest'
 import Toggle from './components/Toggle'
 import prefecturesJson from './sample/api/v1/prefectures.json'
 
 const App = () => (
   <>
-    <h1>都道府県別の総人口推移グラフを表示するSPA</h1>
+    <div style={{width: '100%'}}>
+      <h1>都道府県別の総人口推移グラフを表示するSPA</h1>
 
-    <hr />
+      <hr />
 
-    <Toggle>
-      <PrefecturesJsonTest prefectures={prefecturesJson} />
-    </Toggle>
+      <Toggle>
+        <PrefecturesJsonTest prefectures={prefecturesJson} />
+      </Toggle>
+    </div>
   </>
 )
 export default App
