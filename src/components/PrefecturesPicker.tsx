ef2d0336f1f6c94217fbaf2b1a8b023f57fd6d7d
takoba <plane25@gmail.com>
2025-03-26 03:05:34 +0900

    :sparkles: Inject updating picked prefs to `<CheckboxButton />` in `<PrefecturesPicker />`

--- src/components/PrefecturesPicker.tsx
+++ src/components/PrefecturesPicker.tsx
@@ -1,12 +1,15 @@
+import { useAtom } from 'jotai'
 import React, { JSX } from 'react'
 import CheckboxButton from './CheckboxButton.tsx'
 import styles from './PrefecturesPicker.module.scss'
+import { pickedPrefecturesAtom } from '~/atoms'
 import { Prefecture } from '~/types'
 
 type Props = {
   prefectures: Prefecture[]
 }
 const PrefecturesPicker: React.FC<Props>  = ({prefectures}: Props): JSX.Element => {
+  const [picked, setPicked] = useAtom(pickedPrefecturesAtom)
 
   return (
     <>
@@ -24,6 +27,14 @@ const PrefecturesPicker: React.FC<Props>  = ({prefectures}: Props): JSX.Element 
                   name='prefectures'
                   value={String(prefCode)}
                   className={styles.checkboxButton}
+                  onToggle={
+                    (current) =>
+                      current
+                        ? setPicked(new Set([...picked, pref]))
+                        : setPicked(new Set([
+                          ...Array.from(picked).filter(p => p.prefCode !== prefCode)
+                        ]))
+                  }
                 >
                   <span>{prefName}</span>
                 </CheckboxButton>
