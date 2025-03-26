b1c6a4dbf0d7850f23b217a0299914c959608962
takoba <plane25@gmail.com>
2025-03-26 22:49:09 +0900

    :construction: Test: passed 2 prefectures to `<GraphRender />`

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import 'normalize.css'
 import { JSX } from 'react'
 import populationCompositionPerYearPref1Json
   from './sample/api/v1/population/composition/perYear-prefCode-1.json'
+import populationCompositionPerYearPref2Json
+  from './sample/api/v1/population/composition/perYear-prefCode-2.json'
 import prefecturesJson from './sample/api/v1/prefectures.json'
 import { GraphRender } from '~/components/GraphRender'
 import { PrefecturesPicker } from '~/components/PrefecturesPicker'
@@ -17,6 +19,7 @@ const App = (): JSX.Element => {
         <PrefecturesPicker prefectures={prefecturesJson.result} />
         <GraphRender dataList={[
           [{prefCode: 1, prefName: '北海道'}, populationCompositionPerYearPref1Json.result],
+          [{prefCode: 2, prefName: '青森県'}, populationCompositionPerYearPref2Json.result],
         ]} />
       </div>
     </>
