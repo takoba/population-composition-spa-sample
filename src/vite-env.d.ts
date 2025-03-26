fa6645402b03a749c5a1cfb7a3ae444de7a1c1ef
takoba <plane25@gmail.com>
2025-03-27 03:51:50 +0900

    :wrench: Add server.proxy settings in vite.config.ts
    - add .env.example
    - add types for Vite Env variables in src/vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly API_ORIGIN: string
+  readonly API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
