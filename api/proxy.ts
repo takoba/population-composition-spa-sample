cce4101a7427ab9ecb1b7c916fafe55f3aad00c8
takoba <plane25@gmail.com>
2025-03-27 20:03:47 +0900

    :bug: Fix api/proxy.ts
    - add validation for the required env variables
    - fix passed option obj in `fetch()`
    - fix build url what passed to `fetch()`

--- api/proxy.ts
+++ api/proxy.ts
@@ -2,15 +2,21 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { API_KEY, API_ORIGIN } = process.env
+  if (API_KEY === undefined || API_ORIGIN === undefined) {
+    throw new Error('Unexpected Error: the required env is undefined.')
+  }
 
-  const response = await fetch(`${API_ORIGIN}${req.url}`, {
-    method: req.method,
-    headers: {
-      'Content-Type': 'application/json',
-      'X-API-KEY': API_KEY ?? ''
+  const response = await fetch(
+    `${API_ORIGIN}${new URL(`http://dummy${req.url ?? ''}`).pathname}`,
+    {
+      method: req.method,
+      headers: {
+        'Accept': 'application/json',
+        'X-API-KEY': API_KEY,
+      },
+      body: ['GET', 'HEAD'].includes(req.method ?? '') ? undefined : req.body,
     },
-    body: req.body,
-  })
+  )
 
   res.status(response.status).json(
     await response.json()
